a207a9011f1ae87cab05cd5802b664bb7c2bd664
Brayan <brayan.alexis.gomez@correounivalle.edu.co>
2023-06-24 08:40:28 -0500

    Se suben los archivos

---
+++ test_main.py
@@ -0,0 +1,28 @@
+import pytest
+from ejercicio1 import sum, es_mayor_que, login
+
+def test_sum():
+    assert sum(3,8)==11
+    assert sum(11,2)==33
+    
+def test_es_mayor_que():
+    assert es_mayor_que(10,11)
+
+@pytest.mark.parametrize(
+    "in_x,in_y,esperado",
+    [(3,4,7),
+    (sum(2,1),4,7),
+    (3,sum(3,1),7),
+    (3,4,sum(2,5)),]
+)
+
+def test_sum_param(in_x,in_y,esperado):
+    assert sum(in_x,in_y)==esperado 
+    
+def test_login_pass():
+    login_passes=login("metologia", "/710011C")
+    assert login_passes
+    
+def test_login_fail():
+    login_fails=login("metologia", "/710011M")
+    assert not login_fails
